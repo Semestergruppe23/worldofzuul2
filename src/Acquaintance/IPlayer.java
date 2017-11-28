a320a4ca9efbb28988d5e65da835d1574049fa26
oskar <skakkristiansen@gmail.com>
2017-11-28 15:12:19 +0100

    added interface for the player class

---
+++ src/Acquaintance/IPlayer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Acquaintance;
+
+/**
+ *
+ * @author Oskar
+ */
+public interface IPlayer {
+    
+    void rewardPoints(int points);
+    void addTime(long time);
+    void removeTime(long time);
+    
+}
