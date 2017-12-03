749cf16079f77a7501f04d525b44fbeab4ae96e8
oskar <skakkristiansen@gmail.com>
2017-12-04 00:05:40 +0100

    Added javadoc documentation in the INPC interface for three methods

--- src/Acquaintance/INPC.java
+++ src/Acquaintance/INPC.java
@@ -13,8 +13,26 @@ import java.io.FileNotFoundException;
  */
 public interface INPC {
     
+    /**
+     * Fills arrays with questions and answers read from a .txt file
+     * This file can be changed as needed as long as it continuesly follows this
+     * structure: line: 1: Dialogue, 2: question, 3,4,5: answer options, 6: the actual answer
+     * @throws FileNotFoundException because it fills two arrays containing 
+     * the NPC's questions and answers from a .txt file
+     */
     void fillArraysWithQuestionsAndAnswers() throws FileNotFoundException;
+    
+    /**
+     * returns questions in the order it is written in the .txt file saved in the build of the game
+     * @return a String containing the next question
+     */
     String generateQuestion();
+    
+    /**
+     * checks the answer from the player, case insensitive
+     * @param playerAnswer: is the answer from the player
+     * @return true if the answer is correct, false if not correct
+     */
     boolean checkAnswer(String playerAnswer);
     
 }
