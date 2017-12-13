3a032ed929a54b5cdcaff589b8ecbd51c43d4c94
Niemeijeren <niemeijeren@gmail.com>
2017-12-13 14:44:05 +0100

    MAin menu fra exit

--- src/UserInterface/exitRoomController.java
+++ src/UserInterface/exitRoomController.java
@@ -6,12 +6,19 @@
 package UserInterface;
 
 import static UserInterface.UserInterfaceFacade.business;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -45,6 +52,18 @@ public class exitRoomController {
 
     @FXML
     private void playerMovement(KeyEvent event) {
+    }
+
+    @FXML
+    private void clickMainMenu(ActionEvent event) throws IOException 
+    {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("StartMenu.fxml"));
+        Parent startParent = loader.load(); 
+        Scene startScene = new Scene(startParent);
+        Stage startStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        startStage.setScene(startScene);
+        startStage.show();
     }
 
    
