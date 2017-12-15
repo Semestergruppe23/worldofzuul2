fc565d05333a674c0b533bc20450c889651e0a4f
casha17 <caspha@gmail.com>
2017-12-15 13:18:25 +0100

    Add comments

--- src/Acquaintance/IData.java
+++ src/Acquaintance/IData.java
@@ -6,7 +6,6 @@
 package Acquaintance;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import org.json.JSONException;
 
@@ -16,8 +15,20 @@ import org.json.JSONException;
  */
 public interface IData {
     
+    /**
+     * 
+     * @return  a list of the highscore
+     */
     public List<String> getHighscore();
+    
+    /**
+     * 
+     * @param name of the player
+     * @param score of the player
+     */
     public void addHighscore(String name, int score);
+    
+    
     public void save(IPlayer player);
     public IPlayer load()throws IOException, JSONException;
     
